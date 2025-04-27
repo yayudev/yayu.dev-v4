8cb94f5387e4f83b8c7f989bf2376e5bdc888fe3
Arturo Coronel <contact@yayu.dev>
2025-04-27 01:47:45 -0700

    reset

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 
 import sentry from "@sentry/astro";
@@ -12,10 +12,10 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react(), sentry(), spotlightjs(), icon()],
+    site: "https://example.com",
+    integrations: [mdx(), sitemap(), react(), sentry(), spotlightjs(), icon()],
 
-  vite: {
-    plugins: [tailwindcss()]
-  }
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
